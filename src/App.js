239c91c283f3ebde1796de04cda93ca6a3f35a1a
irguk <igukova@gmail.com>
2023-03-20 23:39:49 +0200

    feat: update app.js file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>Weather App</h1>
+     <footer>This <a href="https://github.com/igukova/my-weather-app">Open-source code</a> , by Gukova Iryna </footer>
     </div>
   );
 }
