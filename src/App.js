c5ee0f39c3653e9192196861a464da56edccdb91
irguk <igukova@gmail.com>
2023-03-21 21:26:53 +0200

    add bootstramp

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Weather App</h1>
-     <footer>This <a href="https://github.com/igukova/my-weather-app">Open-source code</a> , by Gukova Iryna </footer>
+      <div className='container'>
+       <h1>Weather App</h1>
+         <footer>This <a href="https://github.com/igukova/my-weather-app">Open-source code</a> , by Gukova Iryna </footer>
+      </div>
     </div>
   );
 }
