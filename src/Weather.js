3e888398d52abeb291efe04a12218d8938ebbbc5
irguk <igukova@gmail.com>
2023-03-28 23:56:43 +0300

    Update weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -6,26 +6,40 @@ import './Weather.css';
 export default function Weather (){
     return  (
     <div className="Weather">
-        <form><input type="search" placeholder="Enter a city..." className="form-control"/></form>
+        <form>
+            <div className="row">
+                <div className="col-9">
+                  <input type="search" placeholder="Enter a city..." className="form-control"/>
+                 </div>
+            
+                 <div className="col-3">
+                   <input type="Submit" value="Search" className="btn-btn-primary-w-100"/>
+                </div>
+            </div>
+        </form>
        <h1>New York</h1>
        <ul>
         <li>Wendnesday 07:00</li>
         <li>Monstly Cloudly</li>
        </ul>
        <div className="row">
-        <div className="col-6">
-            <img scr="https://ssl.gstatic.com/onebox/weather/64/perty
-            _cloudy.png" alt="Mostly Cloudy"/>
-            6°C
-
-        </div>
-        <div className="col-6">
+          <div className="col-6">
+            <div className="clearfix">
+              <img scr="https://ssl.gstatic.com/onebox/weather/64/perty
+            _cloudy.png" alt="Mostly Cloudy" className="float-left"/>
+              <div className="float-left">
+                 <span className="temperature">6</span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
             <ul>
                 <li>Prasipitation:15%</li>
                 <li>Humidity:72%</li>
                 <li>Wind:13km/h</li>
             </ul>
-        </div>
+           </div>
        </div>
     </div>
         
