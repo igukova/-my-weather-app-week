157c51b36f9577f53e47aa4252db36446c1e647e
irguk <igukova@gmail.com>
2023-04-02 00:48:04 +0300

    Added API integration

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <div className='container'>
        
-       <Weather/>
+       <Weather defaultCity="New York"/>
          <footer>This project was coded by {""} <a href="https://github.com/igukova/" target="">Gukova Iryna</a> {""}  <a href="https://github.com/igukova/my-weather-app">Open-source code</a> </footer>
       </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,48 +1,70 @@
-import React from "react";
+import React,{useState} from "react";
 import './Weather.css';
+import axios from "axios";
 
 
 
-export default function Weather (){
+export default function Weather(props){
+  
+  const[weatherData, setWeatherData]=useState({ ready:false });
+  function hendleResponse(response){
+    console.log(response.data);
+    setWeatherData({
+      ready:true,
+      temperature:response.data.main.temp,
+      humidity:response.data.main.humidity,
+      description: response.data.weather[0].description,
+      wind:response.data.wind.speed,
+      city:response.data.name
+    });
+    
+  }
+  if(weatherData.ready){
     return  (
-    <div className="Weather">
-        <form>
-            <div className="row">
-                <div className="col-9">
-                  <input type="search" placeholder="Enter a city..." className="form-control"/>
-                 </div>
-            
-                 <div className="col-3">
-                   <input type="Submit" value="Search" className="btn-btn-primary-w-100"/>
+      <div className="Weather">
+          <form>
+              <div className="row">
+                  <div className="col-9">
+                    <input type="search" placeholder="Enter a city..." className="form-control"/>
+                   </div>
+              
+                   <div className="col-3">
+                     <input type="Submit" value="Search" className="btn-btn-primary-w-100"/>
+                  </div>
+              </div>
+          </form>
+         <h1>{weatherData.city}</h1>
+         <ul>
+          <li>Wendnesday 07:00</li>
+          <li className="text-kapitalize"> {weatherData.discription}</li>
+         </ul>
+         <div className="row">
+            <div className="col-6">
+              <div className="clearfix">
+                <img scr={weatherData.iconUrl} alt={weatherData.description} className="float-left"/>
+                <div className="float-left">
+                   <span className="temperature">{Math.round(weatherData.temperature)}</span>
+                  <span className="unit">°C</span>
                 </div>
-            </div>
-        </form>
-       <h1>New York</h1>
-       <ul>
-        <li>Wendnesday 07:00</li>
-        <li>Monstly Cloudly</li>
-       </ul>
-       <div className="row">
-          <div className="col-6">
-            <div className="clearfix">
-              <img scr="https://ssl.gstatic.com/onebox/weather/64/perty
-            _cloudy.png" alt="Mostly Cloudy" className="float-left"/>
-              <div className="float-left">
-                 <span className="temperature">6</span>
-                <span className="unit">°C</span>
               </div>
             </div>
-          </div>
-          <div className="col-6">
-            <ul>
-                <li>Prasipitation:15%</li>
-                <li>Humidity:72%</li>
-                <li>Wind:13km/h</li>
-            </ul>
-           </div>
-       </div>
-    </div>
-        
-        
-)
+            <div className="col-6">
+              <ul>
+                  <li>Prasipitation:15%</li>
+                  <li>Humidity:{weatherData.humidity}</li>
+                  <li>{weatherData.wind}</li>
+              </ul>
+             </div>
+         </div>
+      </div>
+  );
+  
+  } else{
+    const apiKey= "efc78e60bc878dfe627bc283d026d166" ;
+    let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(hendleResponse);
+
+    return "Loading...";
+  }
+  
 }
