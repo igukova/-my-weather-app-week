3be272bf1f701ccd021c077375e92b9ded81f579
irguk <igukova@gmail.com>
2023-04-09 05:54:15 +0300

    Added WeatherTemperature.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,13 +1,13 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 
 export default function WeatherInfo(props){
     return ( 
     <div className="WeatherInfo">
-    <h1>{props.data.city}</h1>
-         
+    <h1>{props.data.city}</h1>      
     <ul>
      <li>
         <FormattedDate date={props.data.date} />
@@ -21,14 +21,13 @@ export default function WeatherInfo(props){
              <WeatherIcon code={props.data.icon}/>
             </div>
             <div className="float-left">
-              <span className="temperature">{Math.round(props.data.temperature)}</span>
-              <span className="unit">°C</span>
+              <WeatherTemperature celsius={props.data.temperature}/>
+              
             </div>
          </div>
        </div>
        <div className="col-6">
-         <ul>
-             
+         <ul> 
              <li>Humidity:{props.data.humidity}%</li>
              <li>Wind:{props.data.wind} km/h</li>
          </ul>
