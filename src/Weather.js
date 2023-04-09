abea0b2ad64fcb55b75ae3c715934d0997d26e1b
irguk <igukova@gmail.com>
2023-04-09 22:54:02 +0300

    Added WeatherForecast.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import './Weather.css';
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 
 
@@ -54,7 +55,7 @@ export default function Weather(props){
               </div>
           </form>
           <WeatherInfo data ={WeatherData}/>
-       
+        <WeatherForecast/>
       </div>
   );
   
